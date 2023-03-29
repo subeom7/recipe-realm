7be310edc7f21e825ec228cb5fe675f6c1c15d27
Thinh Truong <thinhtruong4402@gmail.com>
2023-03-29 16:09:18 -0400

    Add bottom navbar and pages

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Recipe-Realm!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Bookmark" component={BookmarkScreen} />
+        <Tab.Screen name="AI" component={AIScreen} />
+        <Tab.Screen name="More" component={MoreScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +34,50 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const Tab = createBottomTabNavigator();
+
+// Home Component
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+// Search Component
+function SearchScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Search Screen</Text>
+    </View>
+  );
+}
+
+// Bookmark Component
+function BookmarkScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Bookmark Screen</Text>
+    </View>
+  );
+}
+
+// AI Component
+function AIScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>AI Screen</Text>
+    </View>
+  );
+}
+
+// More Component
+function MoreScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>More Screen</Text>
+    </View>
+  );
+}

1e85b3432ebb5948728f1da3ca5b1506a9835155
Subeom7 <subeomkwon@gmail.com>
2023-04-08 18:58:42 -0400

    Implement recipe share feature using expo-sharing.

--- src/screens/search/RecipeDetails.js
+++ src/screens/search/RecipeDetails.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import * as Print from 'expo-print';
+import * as Sharing from 'expo-sharing';
+// import { API_KEY } from '@env';
 
 const RecipeDetails = ({ route, navigation }) => {
   const { recipeId } = route.params;
@@ -12,7 +16,7 @@ const RecipeDetails = ({ route, navigation }) => {
   async function fetchRecipeDetails() {
     try {
       const response = await fetch(
-        `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=d09d6982101f4d10968b4c601cd128f5`
+        `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${process.env.API_KEY}`
       );
       if (response.ok) {
         const data = await response.json();
@@ -25,12 +29,56 @@ const RecipeDetails = ({ route, navigation }) => {
     }
   }
 
+  async function shareRecipe() {
+    try {
+      const pdfName = `${recipeDetails.title.replace(/\s+/g, '_')}.pdf`;
+      const pdfUri = await Print.printToFileAsync({
+        html: `
+          <h1>${recipeDetails.title}</h1>
+          <img src="${recipeDetails.image}" width="100%" />
+          <h2>Ingredients</h2>
+          <ul>
+            ${recipeDetails.extendedIngredients
+              .map((ingredient) => `<li>${ingredient.name}</li>`)
+              .join('')}
+          </ul>
+          <h2>Instructions</h2>
+          <ol>
+            ${recipeDetails.analyzedInstructions[0]?.steps
+              .map((step) => `<li>${step.step}</li>`)
+              .join('')}
+          </ol>
+        `,
+      });
+
+      if (!(await Sharing.isAvailableAsync())) {
+        alert('Sharing is not available on this device');
+        return;
+      }
+
+      await Sharing.shareAsync(pdfUri.uri, {
+        mimeType: 'application/pdf',
+        dialogTitle: 'Share Recipe PDF',
+        UTI: 'com.adobe.pdf',
+      });
+    } catch (error) {
+      console.error('Error sharing recipe:', error);
+      alert('Error sharing recipe');
+    }
+  }
+  
+
   if (!recipeDetails) {
     return <Text>Loading...</Text>;
   }
 
   return (
     <ScrollView>
+      <TouchableOpacity onPress={shareRecipe}>
+        <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>
+          Share Recipe
+        </Text>
+      </TouchableOpacity>
       <View>
         <Text>{recipeDetails.title}</Text>
         <Image
